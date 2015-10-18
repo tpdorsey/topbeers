55d960211b28c09b44717ccc3ca350ba81649db5
Terrence Dorsey <terrend@mishu.com>
2015-10-17 23:18:32 -0400

    reports ba250 as markdown table

--- topbeers.py
+++ topbeers.py
@@ -15,7 +15,7 @@ html = myopener.open(url)
 soup = BeautifulSoup(html, 'html.parser')
 
 rows = soup.findAll('tr')
-beer_rows = rows[2:13]
+beer_rows = rows[2:]
 
 top_beers = []
 brewer_states = {}
@@ -24,16 +24,28 @@ for row in beer_rows:
 
   a_tags = row.findAll('td')[1].findAll('a')
 
+  rank = row.findAll('td')[0].text
   beer_name = a_tags[0].text
   beer_page = a_tags[0].get('href')
   brewer_name = a_tags[1].text
   brewer_page = a_tags[1].get('href')
   beer_style = a_tags[2].text
 
-  weighted_rating = row.findAll('b')[1].text
+  remove_string = rank + beer_name + brewer_name + beer_style
 
-  top_beers.append([beer_name, brewer_name, beer_style])
+  raw_abv_wr = row.text.replace(remove_string,"")
 
+  if " / " in raw_abv_wr:
+    abv_wr = raw_abv_wr.replace(" / ","").split("% ABV")
+  else:
+    abv_wr = ["", raw_abv_wr]
+
+  # print "| " + rank + " | " + beer_name + " | " + brewer_name + " | " + beer_style + " | " + abv_wr[0] + " | " + abv_wr[1] + " |"
+
+  top_beers.append([rank, beer_name, brewer_name, beer_style, abv_wr[0], abv_wr[1]])
+
+print "| Rank | Beer | Brewer | Style | ABV | WR |"
+print "| --- | --- | --- | --- | --- | --- |"
 
 for beer in top_beers:
-  print beer[0] + " | " + beer[1] + " | " + beer[2]
+  print "| " + beer[0] + " | " + beer[1] + " | " + beer[2] + " | " + beer[3] + " | " + beer[4] + " | " + beer[5] + " |"
